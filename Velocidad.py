f265f0980c8c9c16cc2c2d7f855d0f442b59c196
DiegoElizaldeUriarte <46821633+DiegoElizaldeUriarte@users.noreply.github.com>
2019-01-22 11:06:15 -0600

    Diego Raúl Elizalde Uriarte, Grupo 03

---
+++ Velocidad.py
@@ -0,0 +1,7 @@
+#Autor: Diego Raul Elizalde Uriarte
+#Calcula velocidad promedio, dado el tiempo y la distancia
+
+d = int(input("Dame la distancia del viaje en kms enteros: "))
+t =  int(input("Dame el tiempo de viaje en hrs enteras: "))
+v = (d/t)
+print("La velocidad promedio es: ",v,"km/h")
