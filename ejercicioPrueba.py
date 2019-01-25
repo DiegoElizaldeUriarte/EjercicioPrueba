67c6139de011742eecbb0f7bdd69d62174702d8e
DiegoElizaldeUriarte <46821633+DiegoElizaldeUriarte@users.noreply.github.com>
2019-01-25 11:27:59 -0600

    a01748756 grupo 2

---
+++ ejercicioPrueba.py
@@ -0,0 +1,13 @@
+a = int(input("Dame el largo de b: "))
+b = int(input("Dame el largo de c: "))
+c = int(input("Dame el largo de d: "))
+d = int(input("Dame el largo de d: "))
+pt = ((d*b)/2)
+st = ((c*d)/2)
+cr = (a-(d+c))
+ac = (a*b)
+t = (pt+st+cr)
+m = 3450
+s = t*m
+print("El área total del terreno es: ",t)
+print("El costo del mismo es: ",s)
